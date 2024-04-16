145b86570a7d26a50aa438d909f478fe02f09c7a
Silicon-Melter <68177054+Silicon-Melter@users.noreply.github.com>
2024-04-16 22:36:12 +0700

    Update train_multi.py

--- multi_drones/train_multi.py
+++ multi_drones/train_multi.py
@@ -30,7 +30,7 @@ tmp_path = os.path.join(dir, "infoLogs")
 new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
 
 checkpoint_callback = CheckpointCallback(
-    save_freq=400,
+    save_freq=600,
     save_path= os.path.join(dir, "checkpoints"),
     name_prefix=f"{formatted_datetime}",
     save_replay_buffer=False,
@@ -40,17 +40,11 @@ checkpoint_callback = CheckpointCallback(
 if __name__ == "__main__":
     envs = SubprocVecEnv([
         lambda: EgaEnv(0, dir), 
-        lambda: EgaEnv(1, dir), 
+        lambda: EgaEnv(1, dir),
         lambda: EgaEnv(2, dir), 
-        lambda: EgaEnv(3, dir), 
-        lambda: EgaEnv(4, dir), 
-        lambda: EgaEnv(5, dir), 
-        lambda: EgaEnv(6, dir), 
-        lambda: EgaEnv(7, dir),
-        lambda: EgaEnv(8, dir), 
-        lambda: EgaEnv(9, dir)
+        lambda: EgaEnv(3, dir)
         ])
     envs = VecMonitor(envs, os.path.join(dir, "infoLogs"))
-    model = PPO("MultiInputPolicy", envs, n_steps=2048, batch_size=64, ent_coef=0.01) #n_steps=2048, batch_size=64)
+    model = PPO("MultiInputPolicy", envs, n_steps=512, batch_size=16, ent_coef=0.01) #n_steps=2048, batch_size=64)
     model.set_logger(new_logger)
     model.learn(total_timesteps=1500000, progress_bar=True, callback=checkpoint_callback, reset_num_timesteps=False)
